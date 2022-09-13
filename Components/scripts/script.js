f332d4f8bf1c8e7fef241a289d9975eedf2d3c24
imGuii <69533374+imGui33@users.noreply.github.com>
2022-09-13 00:43:44 -0400

    j
    a

--- Components/scripts/script.js
+++ Components/scripts/script.js
@@ -7,12 +7,12 @@ const api = {
     units: "metric"
 }
 let inputCity = document.querySelector(".inputSearchCity")
-let submitInput = document.querySelector(".submit")
+let submitInput = document.querySelector(".submit_btn")
 
 
 const searchCoordsCity = async(cidade) => {
     console.log(cidade)
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cidade}&limit=5&appid=${api.key}`)
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cidade}&limit=5&appid=${api.key}&lang=${api.lang}`)
         .then(async(res) => {
             if (!res.ok) {
                 throw new Error(`http error: status ${response.status}`)
@@ -25,7 +25,7 @@ const searchCoordsCity = async(cidade) => {
             let latitude = corpo[0].lat
             let longitude = corpo[0].lon
 
-            console.log(latitude, longitude)
+           
 
             searchResultsCity(latitude, longitude)
         })
@@ -34,7 +34,7 @@ const searchCoordsCity = async(cidade) => {
 
 
 const searchResultsCity = (latitude, longitude) => {
-    fetch(`${api.base}weather?lat=${latitude}&lon=${longitude}&appid=${api.key}`)
+    fetch(`${api.base}weather?lat=${latitude}&lon=${longitude}&appid=${api.key}&lang=${api.lang}`)
     .then((res) => {
         if (!res.ok) {
             throw new Error(`http error: status ${response.status}`)
@@ -53,12 +53,44 @@ const searchResultsCity = (latitude, longitude) => {
 
 const displayResults = (weather) => {
     console.log(weather)
+    let CityName = document.querySelector('.CityName')
+    let temperature = document.querySelector('.temperature')
+    let now_weather= document.querySelector('.now_weather')
+    let humidity = document.querySelector('.humidity')
+    let speed_wind = document.querySelector('.speed_wind')
+
+    // if(weather['weather'][0]['main'] == )
+
+    if(weather['weather'][0]['id'] == 741){ 
+        document.body.classList.remove("sunny")
+        document.body.classList.add("rain")
+    } else {
+        document.body.classList.remove("rain")
+        document.body.classList.add("sunny")
+
+    }
+
+
+    let temperatureFinal = weather['main']['temp']-273.15
+    temperatureFinal = temperatureFinal.toString().substring(0, 2)
+    console.log(temperatureFinal)
+    let temporal = weather['weather'][0]['description'][0].toUpperCase() + weather['weather'][0]['description'].substr(1)
+
+   
+    CityName.innerHTML = `${weather.name}<br> `
+    temperature.innerHTML = `${temperatureFinal} C°<br>`
+
+    now_weather.innerHTML = `${temporal}<br>`
+    humidity.innerHTML = `${weather['main']['humidity']}% <br>`
+    speed_wind.innerHTML = `${weather['wind']['speed']} KM/HR <br>`
+
     
 }   
 
 
 submitInput.addEventListener("click", () =>{
-    // searchCoordsCity(inputCity.value)
+    event.preventDefault()
+    searchCoordsCity(inputCity.value)
     displayResults()
     
 })
